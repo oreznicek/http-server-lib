5309f7d6eb84cf17721240b6b443b5d8a2362115
oreznicek <reznicek.oliver@gmail.com>
2026-04-24 14:31:28 +0200

    Add directory listing example

---
+++ examples/dir_list/main.cpp
@@ -0,0 +1,18 @@
+#include <http/server.hpp>
+
+#include <source_location>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+static fs::path public_dir = fs::path(std::source_location::current().file_name()).remove_filename() / "public";
+
+int main()
+{
+    http::Server srv = http::ServerBuilder()
+        .set_public_dir(public_dir)
+        .enable_directory_listing()
+        .build();
+
+    srv.run();
+}
