886b9e32417ce18899ad0eb1e526182dc922995a
oreznicek <reznicek.oliver@gmail.com>
2026-04-29 17:59:35 +0200

    Change tests log level to Debug + set default test log file

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,5 +1,10 @@
 #include "test_common.hpp"
+#include "logger.hpp"
 
-int main() {
+#include <format>
+
+int main(int argc, char* argv[]) {
+    logger::set_level(logger::Level::Debug);
+    logger::set_log_file(std::format("{}.log", argv[0]));
     return TestRegistry::instance().run_all();
 }
